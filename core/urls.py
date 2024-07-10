59b0f5f6159622adde0b468fe84ce62f16a37b02
Dan Lopatenco <danlopatenco@gmail.com>
2024-07-10 22:42:20 +0300

    First commit

---
+++ core/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, ProductViewSet
+
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'products', ProductViewSet, basename='product')
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    *router.urls
+]
